48fd5a3915b51a98f795a354ded16c518c92ed43
Lenspace <ustbenspace@gmail.com>
2017-11-02 15:22:02 +0800

    注册及登录
    参考lxf，day-10

---
+++ schema.sql
@@ -0,0 +1,48 @@
+-- schema.sql
+-- mysql -u root -p < schema.sql 用来执行sql，创建表
+
+drop database if exists lxf_awesome;
+
+create database lxf_awesome;
+
+use lxf_awesome;
+
+grant select, insert, update, delete on lxf_awesome.* to 'www-data'@'localhost' identified by 'www-data';
+
+create table users (
+    `id` varchar(50) not null,
+    `email` varchar(50) not null,
+    `passwd` varchar(50) not null,
+    `admin` bool not null,
+    `name` varchar(50) not null,
+    `image` varchar(500) not null,
+    `created_at` real not null,
+    unique key `idx_email` (`email`),
+    key `idx_created_at` (`created_at`),
+    primary key (`id`)
+) engine=innodb default charset=utf8;
+
+create table blogs (
+    `id` varchar(50) not null,
+    `user_id` varchar(50) not null,
+    `user_name` varchar(50) not null,
+    `user_image` varchar(500) not null,
+    `name` varchar(50) not null,
+    `summary` varchar(200) not null,
+    `content` mediumtext not null,
+    `created_at` real not null,
+    key `idx_created_at` (`created_at`),
+    primary key (`id`)
+) engine=innodb default charset=utf8;
+
+create table comments (
+    `id` varchar(50) not null,
+    `blog_id` varchar(50) not null,
+    `user_id` varchar(50) not null,
+    `user_name` varchar(50) not null,
+    `user_image` varchar(500) not null,
+    `content` mediumtext not null,
+    `created_at` real not null,
+    key `idx_created_at` (`created_at`),
+    primary key (`id`)
+) engine=innodb default charset=utf8;
